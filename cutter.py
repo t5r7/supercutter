d2c9d06a2d3d0b41f509cae0561b5bfa6aa20361
Tom <git@tomr.me>
2022-03-16 20:42:09 +0000

    well i'll be damned it works

---
+++ cutter.py
@@ -0,0 +1,73 @@
+import json, sys, os
+
+keywords = [
+    "good size",
+    "nice size",
+    "generous"
+]
+
+if(not sys.argv[1]):
+    print("missing arg: video file")
+    quit()
+if(not sys.argv[2]):
+    print("missing arg: subtitle file")
+    quit()
+if(not sys.argv[2]):
+    print("missing arg: output folder")
+    quit()
+
+videoFilePath = sys.argv[1]
+subsFilePath = sys.argv[2]
+outputFolder = sys.argv[3]
+ 
+print("""
+SUPERCUTTER:
+
+Video File: "%s"
+Subs  File: "%s"
+Out Folder: "%s"
+"""%(videoFilePath,subsFilePath,outputFolder))
+
+subsFile = open(subsFilePath, "r")
+subs = json.loads(subsFile.read())
+
+
+# convert from seconds to hh:mm:ss for ffmpeg
+# https://www.geeksforgeeks.org
+# /python-program-to-convert-seconds-into-hours-minutes-and-seconds/
+def hms(seconds):
+    min, sec = divmod(seconds, 60)
+    hour, min = divmod(min, 60)
+    return "%d:%02d:%02d" % (hour, min, sec)
+
+
+# cut the clip using ffmpeg
+# todo: use python ffmpeg implementation rather
+#       than using os.system
+def cut_segment(inputPath,startTime,duration,outputPath):
+    startTime = hms(startTime)
+    os.system('ffmpeg -hide_banner -loglevel error -ss %s -i "%s" -t %s "%s" '%(startTime, inputPath, duration, outputPath))
+
+
+# loop over every caption in the subtitle file
+for caption in subs[0]:
+    if(not caption["text"]):
+        continue
+
+    # check if the caption contains the keywords
+    # https://stackoverflow.com/a/3389611/6325767
+    if any(x in caption["text"] for x in keywords):
+        text = caption["text"]
+        startTime = caption["start"]
+        duration = caption["duration"]
+       
+        textClean = text.encode('ascii', 'ignore') # todo: make this more inclusive
+        outputFile = "%s/%s.mp4"%(outputFolder,textClean)
+
+        print("cutting segment that starts at %s, lasts %s seconds: '%s'"%(startTime, duration, text))
+        cut_segment(videoFilePath, startTime, duration, outputFile)
+
+
+
+# done with you now!!
+subsFile.close()
